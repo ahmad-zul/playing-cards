cb8dd0e199bb35b4bf064afec338e237a67c6c35
Ahmad Zulhafizuddin <zmamat@bousteadgeospatial.com>
2020-01-12 00:59:04 +0800

    Beautify the result page

--- handleSubmit.php
+++ handleSubmit.php
@@ -10,9 +10,14 @@ $validNo = $processor->checkNoOfPlayers($noOfPlayers);
 
 if ($validNo) {
     $cards = $processor->distributeCard($noOfPlayers);
+    echo "Cards has been evenly distributed to each players and below are the results";
+    echo "<br />";
+    echo "=====================================================";
+    echo "<br />";
     for ($i=0; $i<sizeof($cards); $i++) {
         $playerCards = implode(", ",$cards[$i]);
         $playerNo = $i+1;
+
         echo "Player $playerNo: $playerCards";
         echo "<br />";
         echo "<br />";
